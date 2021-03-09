4b7eb4c2209a16b95c0232c1bc181db74163a32c
chip-student <omoram28@gmail.com>
2021-03-09 20:51:39 +0000

    Lista de tareas 1.0

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+Form.propTypes = {
+	inputValue: PropTypes.string,
+	setInputValue: PropTypes.func,
+	setTodos: PropTypes.func,
+	todos: PropTypes.array
+};
+
+function Form(props) {
+	const inputTextValue = e => {
+		props.setInputValue(e.target.value);
+	};
+
+	const submitTodo = e => {
+		if (props.inputValue === "") {
+			alert("Debe ingresar una tarea");
+		} else {
+			e.preventDefault();
+			props.setTodos([
+				...props.todos,
+				{
+					text: props.inputValue,
+					id: props.todos.length
+					// id: Math.floor(Math.random() * 1000)
+				}
+			]);
+			props.setInputValue("");
+		}
+	};
+	return (
+		<div className="input-group mb-2">
+			<input
+				className="form-control"
+				type="text"
+				onChange={inputTextValue}
+				value={props.inputValue}
+			/>
+			<button className="btn btn-primary ml-1" onClick={submitTodo}>
+				<i className="fas fa-plus"></i>
+			</button>
+		</div>
+	);
+}
+TodoList.propTypes = {
+	todos: PropTypes.array,
+	setTodos: PropTypes.func
+};
+
+function TodoList(props) {
+	return (
+		<ul className="list-group">
+			{props.todos.map(todo => (
+				<ListItem
+					text={todo.text}
+					key={todo.id}
+					setTodos={props.setTodos}
+					todos={props.todos}
+					todo={todo}
+				/>
+			))}
+		</ul>
+	);
+}
+ListItem.propTypes = {
+	text: PropTypes.string,
+	id: PropTypes.number,
+	setTodos: PropTypes.func,
+	todos: PropTypes.array,
+	todo: PropTypes.object
+};
+
+function ListItem(props) {
+	//Arrow functions
+	const deleteItem = () => {
+		props.setTodos(
+			props.todos.filter(element => element.id !== props.todo.id)
+		);
+	};
+	return (
+		<li
+			key={props.id}
+			className="form-control d-flex justify-content-between align-items-center">
+			{props.text}
+			<button
+				onClick={deleteItem}
+				type="button"
+				className="btn btn-light btn-sm float right delete"
+				aria-label="Close">
+				<i className="fas fa-times"></i>
+			</button>
+		</li>
+	);
+}
 
 //create your first component
 export function Home() {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row">
+				<div className="col-md-8">
+					<div className="d-flex justify-content-center">
+						<h1>Lista de tareas</h1>
+					</div>
+					<Form
+						todos={todos}
+						setTodos={setTodos}
+						inputValue={inputValue}
+						setInputValue={setInputValue}
+					/>
+					<TodoList setTodos={setTodos} todos={todos} />
+				</div>
+			</div>
 		</div>
 	);
 }
